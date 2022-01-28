57d2b0986e1d95725a65fd7f7550f3a4c5a7c3fb
frontend200tb <frontend200tb@gmail.com>
2022-01-28 20:36:05 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,51 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*':      ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+  const numOfLetters = expr.length / 10; // число букв
+  const arrOfDigits = []; // массив цифр созданный из строки цифр
+  let arrOfDots = []; // массив из точек и тире длиной равной числу букв
+  for (let i = 0; i < numOfLetters; i++) {
+    arrOfDots[i] = ''; // записываем пустую строку в каждый элемент
+  }
+
+  console.log('пустой массив точек и тире', arrOfDots);
+
+  for (let i = 0; i < expr.length; i += 10) {
+      arrOfDigits.push(expr.slice(i, i+10))
+  }
+  for (let i = 0; i < numOfLetters; i++) {
+    for (let j = 0; j < 10; j += 2) {
+      if (arrOfDigits[i][j] === '1' && arrOfDigits[i][j+1] === '0' ) {
+          arrOfDots[i] += '.';
+      }
+      if (arrOfDigits[i][j] === '1' && arrOfDigits[i][j+1] === '1' ) {
+        arrOfDots[i] += '-';
+      }
+      if (arrOfDigits[i][j] === '*') {
+        arrOfDots[i] = '*';
+        break;
+      }
+    }
+  }
+
+  console.log('начальное выражение', expr);
+  console.log('число букв', numOfLetters);
+  console.log('массив цифр', arrOfDigits);
+  console.log('массив точек и тире', arrOfDots);
+
+  const arrOfLetters = arrOfDots.map(item => {
+    return MORSE_TABLE[item]
+  });
+
+  console.log('массив букв', arrOfLetters);
+
+  const result = arrOfLetters.join('');
+  return result;
+
 }
 
 module.exports = {
